a23a371818accec0bf2695ad1783ced16eef5448
NicoPio <nicoportillo@gmail.com>
2026-01-26 19:43:24 +0100

    fix linting issue

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
 	{
-		ignores: ['dist', 'node_modules', '.eslintrc.cjs', 'specs', 'tests'],
+		ignores: ['dist', 'node_modules', '.eslintrc.cjs', 'specs', 'tests', 'public/gif.worker.js'],
 	},
 	js.configs.recommended,
 	...tseslint.configs.recommended,
